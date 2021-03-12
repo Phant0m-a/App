9c3b0ae75a4561efb5468136494bbdfb8397a1d3
Phant0m-a <60280488+Phant0m-a@users.noreply.github.com>
2021-03-12 15:38:02 +0500

    removed one button from biography app fixed 2 vurns and added signature and beta ready now

--- routes/screen.js
+++ routes/screen.js
@@ -5,6 +5,8 @@ const firebase = require('firebase');
 // const firebase = require("firebase/app");
 const firestore = require('firebase/firestore')
 
+firebase.auth.Auth.Persistence.LOCAL;
+
 var firebaseConfig = {
     apiKey: "AIzaSyBHd50N3vrsVyjUYUa-753UnpZQesUHHWU",
     authDomain: "node-web-app-9a6e2.firebaseapp.com",
@@ -27,58 +29,191 @@ var firebaseConfig = {
 //     res.render('screen/index')
 // })
 // test area
-router.get('/hrefReq/:reqs', (req,res) =>{
-    if(req.params.reqs == 'admin'){
-        console.log(req.params.reqs);
-        if(firebase.auth().currentUser){
-            res.redirect('/screen/admin');
-        }else{  
-              res.render('screen/signin');
-        }
-    }
+// router.get('/hrefReq/:reqs', (req,res) =>{
+//     if(req.params.reqs == 'admin'){
+//         console.log(req.params.reqs);
+//         if(firebase.auth().currentUser){
+//             res.redirect('/screen/admin');
+//         }else{  
+//             //change
+//               res.redirect('/screen/signin');
+//         }
+//     }
     
+// })
+
+let count= 0;
+let panel = 0;
+// sign-in get 
+router.get('/signin',(req,res)=>{   
+    // let joined = Date();
+    // console.log(joined);
+     //let timeStamp = firebase.firestore.FieldValue.serverTimestamp();
+   // console.log(firebase.firestore.FieldValue.serverTimestamp());
+// const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+//docRef.update({ updatedAt: timestamp });
+//const todayAsTimestamp = admin.firestore.Timestamp.now();
+//console.log(todayAsTimestamp);
+
+
+// change
+    // var user = firebase.auth().currentUser;
+    // if(firebase.auth().currentUser){
+    //     res.render('screen/admin-panel');
+    // }else{  
+    //       res.render('screen/signin');
+    // }
+    res.render('screen/signin');
+});
+
+// change
+router.post('/signin', async (req,res) => {
+    let  u_email = req.body.email;
+    let u_password = req.body.password;
+    
+  
+  //testing ...
+
+    if(count == 0 && panel == 0 ){
+       // res.render('screen/admin-panel')
+        //signature text == 'some field' or use else if field ..if count ==1 && signature == 'adminSignature '
+        const auth = firebase.auth();
+        try{
+          await  auth.signInWithEmailAndPassword(u_email, u_password).then(resp=>{
+            count = 1;
+            console.log('count: '+count);
+                res.render("screen/admin-panel");
+                 })
+        }
+        catch(err){
+            console.log(err.code); 
+            res.render('screen/signin',{
+                email: req.body.email,
+                errorMessage: 'invalid Email or password!'
+                }); 
+        }
+       
+    }
+    else {
+        res.render('screen/signature');
+    }   
+
+   
+})
+
+// signature route
+router.get('/signature' ,async (req,res) => {
+    const auth = firebase.auth();
+    let signature = req.body.signature;
+    if(count == 1 && panel == 0 && signature == 'admin'){
+        try{
+            await  auth.signInWithEmailAndPassword(u_email, u_password).then(resp=>{
+              count = 1;
+              
+              console.log('count: '+count);
+              console.log('Panel: '+Panel);
+            
+                  res.render("screen/admin-panel");
+                   })
+          }
+          catch(err){
+              console.log(err.code); 
+              res.render('screen/signin',{
+                  email: req.body.email,
+                  errorMessage: 'invalid Email or password!'
+                  }); 
+          }
+    }else if(count == 0 && panel == 0 && signature == 'admin'){
+        try{
+            await  auth.signInWithEmailAndPassword(u_email, u_password).then(resp=>{
+              count = 1;
+              
+              console.log('count: '+count);
+            
+                  res.render("screen/admin-panel");
+                   })
+          }
+          catch(err){
+              console.log(err.code); 
+              res.render('screen/signin',{
+                  email: req.body.email,
+                  errorMessage: 'invalid Email or password!'
+                  }); 
+          }
+    }
+     else{
+            res.send('sorry signature is already being used ~ wait for 24 hours before signing in again');
+    }
+})
+router.post('/signature', async (req,res) => {
+    let  u_email = req.body.email;
+    let u_password = req.body.password;
+    
+    const auth = firebase.auth();
+    let signature = req.body.signature;  
+    
+    console.log(signature);
+
+    if(count == 1 && panel == 0 && signature == 'admin'){
+        try{
+            await  auth.signInWithEmailAndPassword(u_email, u_password).then(resp=>{
+              count = 1;
+              panel = 1;
+              console.log('panel: '+panel);
+              console.log('count: '+count);
+            
+                  res.render("screen/admin-panel");
+                   })
+          }
+          catch(err){
+              console.log(err.code); 
+              res.render('screen/signin',{
+                  email: req.body.email,
+                  errorMessage: 'invalid Email or password!'
+                  }); 
+          }
+    } else if(count == 1 && panel == 1 && signature == 'admin'){
+            res.send('sorry signature is already being used ~ wait for 24 hours before signing in agin');
+    }
+
 })
 
 
-// sign-in get 
-router.get('/signin',(req,res)=>{  
-    var user = firebase.auth().currentUser;
-    if(firebase.auth().currentUser){
-        res.render('screen/admin-panel');
-    }else{  
-          res.render('screen/signin');
-    }
-});
 
 
 //sign-in post
-router.post('/signin',(req,res)=>{
-    let  u_email = req.body.email;
-    let u_password = req.body.password;
-    const auth = firebase.auth();
-    auth.signInWithEmailAndPassword(u_email, u_password).then(resp=>{
+// router.post('/signin',(req,res)=>{
+//     let  u_email = req.body.email;
+//     let u_password = req.body.password;
+//     const auth = firebase.auth();
+//     auth.signInWithEmailAndPassword(u_email, u_password).then(resp=>{
     
-      res.render("screen/admin-panel");
+//       res.render("screen/admin-panel");
       
-    }).catch(err=>{
+//     }).catch(err=>{
 
-        switch(err.code){
-                case "auth/user-not-found":
-                    console.log("User not found");
+//         switch(err.code){
+//                 case "auth/user-not-found":
+//                     console.log("User not found");
                 
-                    break;
-                    default:
-                        console.log("Default");
-        }
+//                     break;
+//                     default:
+//                         console.log("Default");
+//         }
            
-    });
-   
-})
+//     });
+//     // change
+//     res.redirect('/screen/signin')
+// })
 
 // signout
 router.get('/signout/:signout',(req,res)=>{  
     console.log(req.params.signout);
     firebase.auth().signOut();
+    count = 0;
+    panel = 0;
+    console.log('count: '+count);
+    console.log('panel: '+panel);
     res.redirect('/screen/signin');
 });
 
@@ -87,11 +222,13 @@ router.get('/signout/:signout',(req,res)=>{
 
 //admin route
 router.get('/admin', (req,res) =>{
-     if(firebase.auth().currentUser){
-        console.log("user Already logged-in!");
+    
+    //console.log(firebase.firestore.Timestamp.fromMillis());
+    if(firebase.auth().currentUser){
+  //      console.log("user Already logged-in!");
         res.render('screen/admin-panel');
     }else{
-        console.log("user is not logedin")
+  //      console.log("user is not logedin")
         res.redirect('/screen/signin');
     }
   
@@ -104,11 +241,11 @@ router.get('/admin', (req,res) =>{
 
 router.post('/new-page', (req,res)=>{
         // select wheel
-     console.log(req.body.select);
+  //   console.log(req.body.select);
      let Qget = req.body.select;
      if(Qget !== undefined && Qget !== '' && Qget !== 'none')
      {   
-     dbs.collection(Qget).get().then( (snapshot) =>
+     dbs.collection(Qget).orderBy('timeStamp').get().then( (snapshot) =>
     {
         let id_list= [];
        // console.log(snapshot.docs);
@@ -125,8 +262,8 @@ router.post('/new-page', (req,res)=>{
 })
 // delete using id
 router.get('/list/:d_id/:col_name', (req,res) =>{
-    console.log(req.params.d_id);
-    console.log(req.params.col_name);
+    // console.log(req.params.d_id);
+    // console.log(req.params.col_name);
  
     let col_name = req.params.col_name;
     let d_id = req.params.d_id;
@@ -134,7 +271,7 @@ router.get('/list/:d_id/:col_name', (req,res) =>{
    
     dbs.collection(col_name).doc(d_id).delete({});
     
-    dbs.collection(col_name).get().then( (snapshot) =>
+    dbs.collection(col_name).orderBy('timeStamp').get().then( (snapshot) =>
     {
         let id_list= [];
        // console.log(snapshot.docs);
@@ -152,25 +289,36 @@ router.get('/edit/:d_id/:col_name', (req,res)=>{
     let col_name = req.params.col_name;
     let d_id = req.params.d_id;
 
-    res.render('screen/edit-screen',{col_name:col_name,d_id:d_id});
+    //res.render('screen/edit-screen',{col_name:col_name,d_id:d_id});
+    
+    if(firebase.auth().currentUser){
+     //   console.log("user logged-in! safe to move on");
+        res.render('screen/edit-screen',{col_name:col_name,d_id:d_id});
+    }else{
+     //   console.log("user is not logedin")
+        res.redirect('/screen/signin');
+    }
 
 })
 // /edit/CE5NN2BQecrtJ5fOWX42/screen/edited  post 
 
 // other side
 router.post('/edit/edit', (req,res)=>{
-    console.log(req.body.button);
-    console.log(req.body.question);
-    console.log(req.body.tid);
+    // console.log(req.body.button);
+    // console.log(req.body.question);
+    // console.log(req.body.tid);
 
     let d_id = req.body.tid;
     let col_name = req.body.button;
     let q = req.body.question;
     if(q !== '' && q.length >= 10 && q != undefined){
+        let timestamp = Date();
+        console.log(timestamp);
         dbs.collection(col_name).doc(d_id).update({
-            question: q
+            question: q,
+            timeStamp: timestamp
         });
-        dbs.collection(col_name).get().then( (snapshot) =>
+        dbs.collection(col_name).orderBy('timeStamp').get().then( (snapshot) =>
         {
             let id_list= [];
            // console.log(snapshot.docs);
@@ -195,14 +343,17 @@ let questionType = req.body.options;
 let question = req.body.question;
 
 if(question !== undefined && question !== '' && question.length >= 10 ){
+    let timestamp = Date();
+// console.log(timestamp);
     dbs.collection(questionType).add({
-        question: question
+        question: question,
+        timeStamp: timestamp
     });
-    console.log(questionType);
-    console.log(question);
+    // console.log(questionType);
+    // console.log(question);
     res.redirect('/screen/admin');
 }else{
-    console.log('redirect: /screen/admin triggered!...')
+  //  console.log('redirect: /screen/admin triggered!...')
     res.redirect('/screen/admin');
 }
     
@@ -216,3 +367,4 @@ module.exports = router
 // set color scheme of whole app
 // remove extra code
 
+// token => put in cookies...
--- server.js
+++ server.js
@@ -20,6 +20,39 @@ app.use(express.static('public'));
 
 //dotenv.config()
 
+
+// testing stuff
+// var admin = require("firebase-admin");
+// const service_account = require("./node-web-app-9a6e2-firebase-adminsdk-67e0c-fa6238225d.json");
+// admin.initializeApp({
+//     credential: admin.credential.cert(service_account)
+// });
+
+
+// async function getemIl(){
+//     var email=await admin.auth().getUserByEmail('admin@admin.com');
+//     console.log(email);
+// }
+// getemIl();
+
+
+
+// let c_user = firebase.auth().currentUser;
+// let c_uid = c_user.uid;
+ let uid = 'start App';
+//    console.log(c_user);
+    console.log(uid);
+// admin.auth().createCustomToken(uid).then( (customToken) => {
+//     console.log(customToken);
+// }).catch((err) => {
+//     console.log('error creating shitty token!', err);
+//     console.log(err.code);
+// })
+
+
+// 
+
+
 // All routes
 app.use('/', indexRouter)
 app.use('/screen', screenRouter)
